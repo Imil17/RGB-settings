0f422b1d0e22a2a46272f3725ab35856959cacb0
Emil <mr.eaminev@yandex.ru>
2022-03-05 01:49:42 +0500

    работает

--- RGB settings/ViewController.swift
+++ RGB settings/ViewController.swift
@@ -8,12 +8,53 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var colorRectangle: UIView!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    @IBOutlet var labelRed: UILabel!
+    @IBOutlet var labelGreen: UILabel!
+    @IBOutlet var labelBlue: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        colorRectangle.layer.cornerRadius = 20
+        
+        labelRed.text = "\((round(redSlider.value * 100) / 100))"
+        labelGreen.text = "\((round(greenSlider.value * 100) / 100))"
+        labelBlue.text = "\((round(blueSlider.value * 100) / 100))"
     }
-
+    
+    func changeColor() {
+        let redColor = CGFloat(redSlider.value)
+        let greenColor = CGFloat(greenSlider.value)
+        let blueColor = CGFloat(blueSlider.value)
+        
+        colorRectangle.backgroundColor = UIColor(
+            red: redColor,
+            green: greenColor,
+            blue: blueColor,
+            alpha: 1.0)
+    }
+    
+    @IBAction func redSliderAction() {
+        labelRed.text = "\((round(redSlider.value * 100) / 100))"
+        changeColor()
+    }
+    
+    @IBAction func greenSliderAction() {
+        labelGreen.text = "\((round(greenSlider.value * 100) / 100))"
+        changeColor()
+    }
+    
+    @IBAction func blueSliderAction() {
+        labelBlue.text = "\((round(blueSlider.value * 100) / 100))"
+        changeColor()
+    }
 
 }
 
